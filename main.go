7b0e3b05d81aba73a4fd0e3798fd34a38f459873
otisnado-i-strategies <otisnado@i-strategies.tech>
2021-12-27 13:47:47 -0600

    adding POST method to add new album

--- main.go
+++ main.go
@@ -26,9 +26,23 @@ func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
 }
 
+// postAlbums adds an album from JSON received in the request body.
+func postAlbums(c *gin.Context) {
+	var newAlbum album
+	// call BindJSON to bind the received JSON to album
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+
+	// add new album to the slice
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
+}
+
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
+	router.POST("/albums", postAlbums)
 
 	router.Run("localhost:8080")
 }
