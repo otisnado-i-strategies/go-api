50fa2371e5b752b0c11d847e3fa7d5d07a65433b
otisnado-i-strategies <otisnado@i-strategies.tech>
2021-12-27 15:01:21 -0600

    adding console  log for requests

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -24,6 +25,7 @@ var albums = []album{
 // get albums response with the list of all albums as JSON
 func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
+	fmt.Println("################## Response with list of all albums ##################")
 }
 
 // postAlbums adds an album from JSON received in the request body.
@@ -36,12 +38,31 @@ func postAlbums(c *gin.Context) {
 
 	// add new album to the slice
 	albums = append(albums, newAlbum)
+	fmt.Println("################## New album was added ##################")
 	c.IndentedJSON(http.StatusCreated, newAlbum)
+}
+
+// getAlbumByID locates the album whose ID value matches the id
+// parameter sent by the client, then returns that album as a response.
+func getAlbumByID(c *gin.Context) {
+	id := c.Param("id")
+
+	// Loop over the list of albums, looking for
+	// an album whose ID value matches the parameter.
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			fmt.Println("################## Response with album requested ##################")
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
 }
 
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
+	router.GET("/albums/:id", getAlbumByID)
 	router.POST("/albums", postAlbums)
 
 	router.Run("localhost:8080")
